64e50b0c69660d522aa730ad2af0ab17c88ea816
Aaron Erlandson <wl7521ol@go.minnstate.edu>
2022-05-02 13:15:34 -0500

    Might get it to work

--- Google Script.R
+++ Google Script.R
@@ -1,1 +1,15 @@
-gd
+install.packages('googlesheets4')
+library(googlesheets4)
+#Read google sheets data into R
+read_sheet('https://docs.google.com/spreadsheets/d/1J9-ZpmQT_oxLZ4kfe5gRvBs7vZhEGhSCIpNS78XOQUE/edit#gid=0')
+
+
+
+install.packages("tidyverse")
+install.packages("googlesheets4")
+install.packages("devtools")
+library(devtools)
+devtools::install_github("UrbanInstitute/urbnmapr")
+library(googlesheets4)
+library(tidyverse)
+library(urbnmapr)
